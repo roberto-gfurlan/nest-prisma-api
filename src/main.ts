8810950e3f11f7576bd830347cbbcabfe43955d6
roberto furlan <furlanrgf@gmail.com>
2024-10-03 20:00:21 -0300

    config dev env

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(3000);
+  await app.listen(3000); 
 
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+bootstrap()
