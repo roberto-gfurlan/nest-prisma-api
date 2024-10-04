aedec914711aa8933df0dc32d5d422d4ac5450af
roberto furlan <furlanrgf@gmail.com>
2024-10-04 15:57:47 -0300

    Refactor EnvConfigService to implement EnvConfig interface and use ConfigService for environment configuration

--- src/shared/infra/env-config/env-config.service.ts
+++ src/shared/infra/env-config/env-config.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { EnvConfig } from './env-config.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+
+  constructor(private ConfigService: ConfigService) {}
+
+  getAppPort(): number {
+    
+    return Number(this.ConfigService.get<number>('PORT'));
+  }
+  getNodeEnv(): string {
+
+    return String(this.ConfigService.get<string>('NODE_ENV'));
+  }
+}
